63b9a44d9d67a66cfaa35cba361e2028615e4a45
Chris Duong <ctd4wa@virginia.edu>
2024-11-22 14:09:32 -0500

    test: adds some tests to check functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('clicking a number updates the screen correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText("7"));
+  fireEvent.click(screen.getByText("5"));
+  const screenElement = screen.getByText(/75/);
+  expect(screenElement).toBeInTheDocument();
+});
+
+test('clicking operations updates the display correctly', () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("7"));
+  fireEvent.click(screen.getByText("5"));
+  fireEvent.click(screen.getByText("+"));
+  fireEvent.click(screen.getByText("3"));
+  fireEvent.click(screen.getByText("="));
+  const screenElement = screen.getByText(/78/); // 75 + 3 = 78
+  expect(screenElement).toBeInTheDocument();
+});
+
+test('clicking equals calculates the result', () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("7"));
+  fireEvent.click(screen.getByText("5"));
+  fireEvent.click(screen.getByText("+"));
+  fireEvent.click(screen.getByText("3"));
+  fireEvent.click(screen.getByText("="));
+  const screenElement = screen.getByText(/78/); // 75 + 3 = 78
+  expect(screenElement).toBeInTheDocument();
+});
+
+test('clicking "+-" inverts the number', () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("7"));
+  fireEvent.click(screen.getByText("5"));
+  fireEvent.click(screen.getByText("+"));
+  fireEvent.click(screen.getByText("3"));
+  fireEvent.click(screen.getByText("=")); // 75 + 3 = 78
+  fireEvent.click(screen.getByText("+-")); // Inverts the result
+  const screenElement = screen.getByText(/-78/); // Result should be -78
+  expect(screenElement).toBeInTheDocument();
+});
+
+test('handles division by zero', () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("5"));
+  fireEvent.click(screen.getByText("/"));
+  fireEvent.click(screen.getByText("0"));
+  fireEvent.click(screen.getByText("="));
+  const screenElement = screen.getByText(/Can't divide with 0/);
+  expect(screenElement).toBeInTheDocument();
 });
